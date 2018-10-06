cca66d39948e66be374c4ad3d2fc5357137ebf45
Venonatt <42704102+Venonatt@users.noreply.github.com>
2018-10-06 13:54:23 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,83 +1,20 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '.'
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Server Dragons Craft`,"http://twitch.tv/S-F")
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
-});
-
-
-const config = require('./configs.json');
-
-
-const size    = config.colors;
-const rainbow = new Array(size);
-
-for (var i=0; i<size; i++) {
-  var red   = sin_to_hex(i, 0 * Math.PI * 2/3); // 0   deg
-  var blue  = sin_to_hex(i, 1 * Math.PI * 2/3); // 120 deg
-  var green = sin_to_hex(i, 2 * Math.PI * 2/3); // 240 deg
-
-  rainbow[i] = '#'+ red + green + blue;
-}
-
-function sin_to_hex(i, phase) {
-  var sin = Math.sin(Math.PI / size * 2 * i + phase);
-  var int = Math.floor(sin * 127) + 128;
-  var hex = int.toString(16);
-
-  return hex.length === 1 ? '0'+hex : hex;
-}
-
-let place = 0;
-const servers = config.servers;
-
-function changeColor() {
-  for (let index = 0; index < servers.length; ++index) {        
-    client.guilds.get(servers[index]).roles.find('name', config.roleName).setColor(rainbow[place])
-        .catch(console.error);
-        
-    
-    
-    if(place == (size - 1)){
-      place = 0;
-    }else{
-      place++;
-    }
-  }
-}
-
-
-
-client.on('ready', () => {
-  console.log('Bot Is Online')
-  if(config.speed <60.000){console.log("The minimum speed is 60.000, if this gets abused your bot might get IP-banned"); process.exit(1);}
-  setInterval(changeColor, config.speed);
-});
-
-
-
+console.log("Welcome Again !");
+ 
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+[ ${member}  ] هلا والله
+❤ ممكن تدخل سيرفر حبي
+       [ https://discord.gg/yEJZt8G ] 
+الدعووة خاصة لك ... 
+السيرفر للتصميم بوت خاص فيك مجانا 24/24 ساعة 😘
+وتربح فلوس عبر بايبال مضمون %100
+**`) 
+}).catch(console.error)
+})
 
 client.login(process.env.BOT_TOKEN);
