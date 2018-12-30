9e3e2a6410f84af33f53076e442cb89a5b9358c0
Venonatt <42704102+Venonatt@users.noreply.github.com>
2018-12-30 14:54:36 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,26 +9,13 @@ client.on("guildMemberAdd", member => {
 **
  هلا والله
 ❤ ممكن تدخل سيرفر حبي
-                     [ https://discord.gg/bqz8NPq ] 
+                     [ https://discord.gg/WPzRQ3F ] 
 الدعووة خاصة لك ... [ ${member}  ]
  سيرفر تضيف أشخاص يعطونك كريدت وفلوس وأشياء زي كذا😘
  مضمون %100
 **`) 
 }).catch(console.error)
-})
+});
 
-client.on("guildMemberAdd", member => {
-  member.createDM().then(function (channel) {
-  return channel.send(` 
-**
- هلا والله
-❤ ممكن تدخل سيرفر حبي
-                     [ https://discord.gg/WPzRQ3F ] 
-الدعووة خاصة لك ... [ ${member}  ]
-سيرفر يحتاج دعم  حبي 
- مضمون %100
-**`) 
-}).catch(console.error)
-})
 
 client.login(process.env.BOT_TOKEN);
